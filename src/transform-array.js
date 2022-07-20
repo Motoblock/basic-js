d25e769eae81a5a2a6f45e1f70c83a9f0c35ae7b
Ludmila <milasasha@gmail.com>
2022-07-21 00:10:44 +0300

    feat: update code transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,33 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  if(!arr) return;
+ if(!arr)  throw new Error("'arr' parameter must be an instance of the Array!");
+
   if (!Array.isArray(arr)) {
-    throw new Error('\'arr\' parameter must be an instance of the Array!');
-}
-
-
-  const yslovie = ['--discard-next','--discard-prev','--double-next','--double-prev'];
-  console.log(arr);
-  let transformArray = arr;
-  //let arr2 = [];
-  for(let i in arr) {
- //   let what = arr.indexOf(i);
- 
-       console.log(arr[i]);
-      if(arr[i] === yslovie[0]) { console.log(i);  transformArray[i] = ''; transformArray[i + 1] = '';} //transformArray = transformArray.filter(item => item !==  yslovie[0]);}//transformArray.splice((i + 1),1); }
-      else if(arr[i] === yslovie[1]) {console.log(i); transformArray[i] = ''; transformArray[i - 1] = '';}//transformArray = transformArray.filter(item => item !==  yslovie[1]);}
-      else if(arr[i] === yslovie[2]) {console.log(i);  transformArray[i] = arr[i + 1]; }
-      else if(arr[i] === yslovie[3]) {console.log(i);  transformArray[i] = arr[i - 1]; }
-      console.log(transformArray);
-
+    throw new Error("'arr' parameter must be an instance of the Array!");
   }
-  transformArray = transformArray.filter(item => item !==  '');
-  console.log(transformArray);
-  return transformArray;
+  if(arr.length === 0) return arr;
+
+  let k = -10, m = '';
+  const yslovie = ['--discard-next','--discard-prev','--double-next','--double-prev'];
+ 
+  let transformArray = [];
+  let arr2 = [];
+  for(let i = 0; i<arr.length; i++) {
+
+      if(arr[i] === yslovie[0]) {  k = i + 1; m = undefined; transformArray[i] = undefined; transformArray[i + 1] = undefined;} //transformArray = transformArray.filter(item => item !==  yslovie[0]);}//transformArray.splice((i + 1),1); }
+      else if(arr[i] === yslovie[1]) { transformArray[i] = undefined; transformArray[i - 1] = undefined;}//transformArray = transformArray.filter(item => item !==  yslovie[1]);}
+      else if(arr[i] === yslovie[2]) {  transformArray[i] = arr[i + 1]; }
+      else if(arr[i] === yslovie[3]) {
+        if(k == i - 1) transformArray[i] = m;
+        else transformArray[i] = arr[i - 1]; }
+      else {
+        if(k !== i ) {transformArray.push(arr[i]); }
+        else {transformArray.push(undefined); };
+      }
+  }
+  arr2 = transformArray.filter(function (el) {
+    return el != null ;
+  });
+ 
+  return arr2;
 }
 
 module.exports = {
   transform
 };
-transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])
+//transform([1, ,2, 3, '--discard-next', 1337, '--double-prev', 4, 5]);
+//transform([undefined, 1, undefined, 5]);
+//transform(undefined);
+//transform([1, 2, 3, 4, 5 ]);
+
