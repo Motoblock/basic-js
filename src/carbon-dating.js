8d007d1edc96f510995e3a115cd40632b9167ed1
Ludmila <milasasha@gmail.com>
2022-07-20 11:20:11 +0300

    feat: add cod in carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!sampleActivity || +sampleActivity < 0 || +sampleActivity > 8999 || sampleActivity === undefined ) return false;
+
+  if (typeof sampleActivity == 'string' && +sampleActivity !== NaN) {
+    let res = Number(Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD)));
+    return (res > 0 && res !== Infinity && res !== undefined)?res: false ;
+  }
+  return false;
 }
 
 module.exports = {
   dateSample
 };
+
+dateSample('15.1');
