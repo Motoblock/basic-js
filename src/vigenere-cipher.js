a9338dd7836cdcbc98a6318ba286c4d5800b9041
Ludmila <milasasha@gmail.com>
2022-10-12 18:42:51 +0300

    feat: vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+var   alphabet = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z'
+]
 class VigenereCipheringMachine {
+
   contructor(ttype = true) {
     this.ttype = ttype;
   }
-  alphabet = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z'
-]
-
 
   encrypt(message, key) {
     if (message == undefined || key == undefined || message === '') {
@@ -36,28 +36,29 @@ class VigenereCipheringMachine {
     }
     key = key.toUpperCase();
     console.log(key);
-     let keystr = '', messtr = ''; let i = 2;
+     let keystr = '', messtr = ''; //let i = 2;
      messtr = message.toUpperCase().split("");
     //  if(this.ttype != true) messtr = messtr.reverse();
-     
-     while(keystr.length < message.length) {
-      keystr = key.repeat(i);
-      i++;
-    }
-   
+
+    //  while(keystr.length < message.length) {
+    //   keystr = key.repeat(i);
+    //   i++;
+    // }
+
     let index = [];
     let countstr = 0;
+   // console.log(messtr);
      for(let i = 0; i < messtr.length; i++) {
       if (countstr === key.length) {
-        countstr = 0;
-      }
-   let tmp = (alphabet.charCodeAt(messtr[i]) + alphabet.charCodeAt(key[countstr])) % 26;
-   console.log(tmp);
-    index.push(alphabet.charCodeAt(tmp));
-    countstr++;
+          countstr = 0;
+        }
+        let tmp = (message.charCodeAt(i) + key.charCodeAt(countstr)) % 26;
+        console.log(tmp);
+        index.push(alphabet[tmp]);
+        countstr++;
+      
   //  i = ++i % key.length
     //return charCodeAt(index);
-   
   }
   if(this.ttype != true) index.reverse();
   index.join('');
@@ -101,8 +102,11 @@ class VigenereCipheringMachine {
 
   }
 }
-  const user = new VigenereCipheringMachine('attack at dawn!','alphonse');
-console.log(user.encrypt);
+
 module.exports = {
   VigenereCipheringMachine
 };
+
+  // const user = new VigenereCipheringMachine('attack at dawn!','alphonse');
+  const user = new VigenereCipheringMachine();
+console.log(user.encrypt('attack at dawn!','alphonse'));
