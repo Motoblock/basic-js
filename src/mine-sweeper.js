658eddcc23d906003c9cb33b8e7855ad4adeea59
Ludmila <milasasha@gmail.com>
2022-10-14 22:24:29 +0300

    feat: mine-sweeper finally!!!!!!

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -26,34 +26,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  ];
 function minesweeper(matrix) {
   if (matrix.length == 0) {return []}
- // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-  //console.log(matrix); 
-  
-  let newmatrix = [[0,0,0],[0,0,0],[0,0,0]]; //matrix.fill(0);
-  //let newmatrix = [...Array(matrix.length)].map(e => Array(matrix[0].length).fill(0))
-  
+
+ // let newmatrix = [[0,0,0],[0,0,0],[0,0,0]]; //matrix.fill(0);
+  let newmatrix =  [...Array(matrix.length)].map(val => Array(matrix[0].length).fill(0));
+
   for(let i = 0; i < matrix.length; i++) {
     for(let j = 0; j < matrix[i].length; j++)
     {
      if (matrix[i][j] == true) {
-   //   if (!(i == 1 && j == 1)) {
+
       let index = [[i - 1, j - 1], [i - 1, j], [i - 1, j + 1],
-                   [i, j - 1] , [i,j],[i , j + 1], 
-                   [i + 1, j - 1],[i + 1, j], [j + 1, j + 1]];
+                   [i, j - 1],     [i, j],     [i , j + 1],
+                   [i + 1, j - 1], [i + 1, j], [j + 1, j + 1]];
+                   let count2 = 1;
       for (let [x, y] of index) {
 
-        //if ((x >= 0 && x < matrix.length) &&  (y >= 0 && y < matrix[i].length)) {
-          console.log('x>=',x,' x < ',matrix.length,'y >=',y,'matrix[i].length <',matrix[i].length);
           if ((x >= 0 && x < matrix.length) &&  (y >= 0 && y < matrix[i].length)) {
-          newmatrix[x][y]++;
+              //console.log('x = ',x,'y =',y,'count2=',count2);
+          if(!(i==x && y==j)) newmatrix[x][y]++;
         }
       }
     }
-
-    }
-      console.log(newmatrix[i]);
   }
+}
+  // newmatrix.forEach(element => {
+  //   console.log(element);
+  // });
   return newmatrix;
 }
 
